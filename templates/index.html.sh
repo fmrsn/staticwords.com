d79b26956a3df231fc0b82e8a860c688b772065b
F. Emerson <fmrsn@fmrsn.com>
2022-04-26 15:26:40 -0300

    Add main stylesheet

--- templates/index.html.sh
+++ templates/index.html.sh
@@ -6,8 +6,9 @@ cat <<EOF
 <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 <meta name="author" content="F. Emerson">
 <meta name="description" content="The thoughts and writings of F. Emerson.">
-<link rel="shortcut icon" href="/favicon.png">
 <link rel="canonical" href="https://$SITE" />
+<link rel="shortcut icon" href="favicon.png">
+<link rel="stylesheet" href="main.css" />
 <title>Static Words</title>
 <article>
 	<h1>Static Words is under construction.</h1>
