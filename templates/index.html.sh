5dd80f2939d2816d5c25b16c724dff1de2cd48a2
F. Emerson <fmrsn@fmrsn.com>
2022-04-27 17:12:50 -0300

    Add support for margin notes

--- templates/index.html.sh
+++ templates/index.html.sh
@@ -13,7 +13,36 @@ cat <<EOF
 <link rel="stylesheet" href="main.css" />
 <title>Static Words</title>
 <article>
-	<p>Static Words is under construction.</p>
+
+<p>abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefgh
+
+<p>Id reprehenderit dignissimos alias voluptas autem. Dolor dolorem tenetur consequatur excepturi optio provident temporibus. Dolores provident ratione commodi debitis quo. Voluptatem eos minus commodi quia fugiat voluptatem modi vitae. Numquam magni inventore consectetur fugiat adipisci. Voluptas voluptatem libero a deserunt molestiae aut sit vel.
+
+<p>Beatae quae iure natus. Voluptatem reiciendis saepe qui similique sint velit. Facilis deserunt libero quo. Error omnis odit aliquid quis voluptatem quas ut asperiores. Maxime quam corrupti maiores cum enim incidunt labore. Quia tempora ut commodi suscipit voluptatibus incidunt inventore quibusdam.
+
+<p>Officiis sint omnis inventore et corporis. Reiciendis laboriosam porro accusamus vel itaque deserunt modi iure. Aut dignissimos reiciendis quaerat ipsum quam maiores eaque sed.
+
+<p>Ut ratione necessitatibus voluptatem in est. Fugiat necessitatibus provident sit possimus ducimus ipsam excepturi inventore. Omnis culpa ut facere corrupti nulla perferendis. Id distinctio soluta quia perferendis. Velit voluptatem quia consequatur earum quia explicabo perferendis dolore.
+
+<aside>
+<p>Saepe dicta dolorum voluptatem enim nostrum doloribus qui. Et rerum voluptatum officia voluptatum cum. Possimus vero esse eos pariatur voluptatem excepturi quia.
+</aside>
+
+<p>Minima omnis velit suscipit. Veritatis unde ipsam ut culpa non quidem illo ipsum. Aut blanditiis fugit voluptatem doloremque.
+
+<p>Provident dolorum aut vitae aut corporis. In rerum animi dolores recusandae et est qui. Quas vel non est labore. Aliquid dignissimos esse architecto ipsum laudantium eaque aspernatur ullam. Quia voluptatem numquam quam itaque.
+
+<aside>
+<p>
+Dolores saepe nam beatae et qui itaque. Aspernatur repellendus sint quo quos amet. Labore officiis blanditiis nam corporis quia similique deserunt. Non magnam cum nisi cumque voluptatum omnis. Dolorum vero ipsa ut placeat vel animi.
+</aside>
+
+<p>
+Aut ut non est iure. Aperiam et labore quo architecto possimus expedita sequi totam. Ea quo reprehenderit sed quis nesciunt.
+
+<p>
+Quisquam adipisci porro magni autem nesciunt delectus illum animi. Culpa possimus velit id quasi qui eaque. Est est consequatur labore officiis nobis. Ab possimus a ab assumenda ut modi. Dolor illo a assumenda voluptatem autem et sequi iusto. Quo sed aperiam vitae quas.
+
 </article>
 </html>
 EOF
